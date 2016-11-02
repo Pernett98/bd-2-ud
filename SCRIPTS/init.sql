bc0d2a33c17fea293c0488e48ae6a0336ee210b9
Sebastian Pernett <pernett98@gmail.com>
2016-11-02 14:44:27 -0500

    Trabajando en DML "falta conductores, volquetas,ventas y entradas"

--- SCRIPTS/init.sql
+++ SCRIPTS/init.sql
@@ -23,6 +23,17 @@
 @ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/DDL/VENTAS_ENTRADAS.sql'
 @ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/DDL/ENTRADAS.sql'
 
+-- TRIGGERS
+@ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/TRIGGERS/COLORES_VOLQUETAS_TR.sql'
+@ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/TRIGGERS/MARCAS_VOLQUETAS_TR.sql'
+@ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/TRIGGERS/MODELOS_VOLQUETAS_TR.sql'
+
+
 -- DML
 
 @ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/DML/EMPRESAS_DML.sql'
+@ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/DML/COLORES_VOLQUETAS_DML.sql'
+@ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/DML/MARCAS_VOLQUETAS_DML.sql'
+@ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/DML/MODELOS_VOLQUETAS_DML.sql'
+@ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/DML/CONDUCTORES_DML.sql'
+@ '/home/sebastian/Documentos/Poli/BD2/bd-2-ud/SCRIPTS/DML/VOLQUETAS_DML.sql'
