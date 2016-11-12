eed90478560db1bb3e52cc0fdac51eb260de46a4
Sebastian Pernett <pernett98@gmail.com>
2016-11-12 06:09:22 -0500

    scripts objetuales

---
+++ SCRIPTS/post_init.sql
@@ -0,0 +1,28 @@
+-- DDL
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DDL/USUARIOS.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DDL/EMPRESAS.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DDL/COLORES_VOLQUETAS.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DDL/MARCAS_VOLQUETAS.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DDL/MODELOS_VOLQUETAS.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DDL/VOLQUETAS.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DDL/CONDUCTORES.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DDL/VENTAS_ENTRADAS.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DDL/ENTRADAS.sql'
+
+-- TRIGGERS
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/TRIGGERS/COLORES_VOLQUETAS_TR.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/TRIGGERS/MARCAS_VOLQUETAS_TR.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/TRIGGERS/MODELOS_VOLQUETAS_TR.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/TRIGGERS/VENTAS_ENTRADAS_TR.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/TRIGGERS/ENTRADAS_TR.sql'
+
+-- DML
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DML/USUARIOS_DML.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DML/EMPRESAS_DML.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DML/COLORES_VOLQUETAS_DML.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DML/MARCAS_VOLQUETAS_DML.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DML/MODELOS_VOLQUETAS_DML.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DML/VOLQUETAS_DML.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DML/CONDUCTORES_DML.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DML/VENTAS_ENTRADAS_DML.sql'
+@ '/home/sebastian/Documentos/BD2/bd-2-ud/SCRIPTS/DML/ENTRADAS_DML.sql'
